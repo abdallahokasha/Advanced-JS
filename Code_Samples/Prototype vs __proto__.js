5ef5f97a708631591bd5e20b46706fb4617096a9
abdallahokasha <okasha.fci@gmail.com>
2019-09-11 15:47:53 +0200

    Edited the prototype and __proto__ example.

--- Code_Samples/Prototype vs __proto__.js
+++ Code_Samples/Prototype vs __proto__.js
@@ -1,36 +1,65 @@
 const Car = function(color) {
   this.color = color;
 };
-
+console.log("Car: ");
 console.dir(Car);
 
 const redCar = new Car("red");
 const blueCar = new Car("blue");
 
+console.log("blueCar: ");
 console.dir(blueCar);
-console.dir("redCar: ", redCar);
+console.log("redCar: ");
+console.dir(redCar);
 
-console.log(Car.prototype === redCar.__proto__);
-console.log(Car.prototype.constructor === redCar.__proto__.constructor);
+console.log(
+  "(Car.prototype === redCar.__proto__) ? ",
+  Car.prototype === redCar.__proto__
+);
+console.log(
+  "(Car.prototype.constructor === redCar.__proto__.constructor) ? ",
+  Car.prototype.constructor === redCar.__proto__.constructor
+);
 // ----
 
 class Person {
   constructor(name) {
     this.name = name;
   }
+  sayName() {
+    console.log(`I am ${this.name}`);
+  }
 }
 
-class Employee extends Person {}
+class Employee extends Person {
+  constructor(name) {
+    super(name);
+  }
+}
+
 console.dir(Person);
 console.dir(Employee);
 
-console.log("Person === Employee.__proto__ ? ", Person === Employee.__proto__);
 console.log(
-  "Person.prototype.constructor === Employee.prototype.constructor ? ",
+  "(Person === Employee.__proto__) ? ",
+  Person === Employee.__proto__
+);
+console.log(
+  "(Person.prototype.constructor === Employee.prototype.constructor) ? ",
   Person.prototype.constructor === Employee.prototype.constructor
 );
 
 console.log(
-  "Employee.__proto__.prototype.constructor === Person.prototype.constructor ? ",
-  Employee.__proto__.prototype.constructor === Person.prototype.constructor
+  "(Person.prototype.constructor === Employee.__proto__.prototype.constructor) ? ",
+  Person.prototype.constructor === Employee.__proto__.prototype.constructor
+);
+
+// what extends do !
+console.log(
+  "(Person === Employee.__proto__) ? ",
+  Person === Employee.__proto__
+);
+console.log(
+  "(Person.prototype === Employee.prototype.__proto__) ? ",
+  Person.prototype === Employee.prototype.__proto__
 );
