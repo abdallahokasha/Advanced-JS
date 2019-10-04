da9d253b92e5c4cfc4bf47489d7c8fce9e16bd99
abdallahokasha <okasha.fci@gmail.com>
2019-10-04 17:06:34 +0200

    Added stopwatch example.
    - Added stopwatch as example for Abstraction.
    - Added code sample for Enumerables vs Iterables.

--- Code_Samples/Abstraction.js
+++ Code_Samples/Abstraction.js
@@ -31,3 +31,76 @@ console.log("circle: ", circle);
 // circle.defaultLocation = 1; // Invalid location
 circle.defaultLocation = { x: 1, y: 1 };
 console.log("circle: ", circle);
+
+// ======================================
+
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
+
+  this.start = function() {
+    if (running) throw new Error("Stopwatch has already started.");
+    running = true;
+    startTime = new Date();
+  };
+
+  this.stop = function() {
+    if (!running) throw new Error("Stopwatch is not started.");
+    running = false;
+    endTime = new Date();
+
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+
+    duration += seconds;
+  };
+
+  this.reset = function() {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+
+  Object.defineProperty(this, "duration", {
+    get: function() {
+      return duration;
+    }
+  });
+}
+
+const stopwatch = new Stopwatch();
+
+console.log("stopwatch: ", stopwatch);
+
+// ##### Enumerable vs Iterable #####
+
+if (stopwatch.start)
+  console.log(
+    "stopwatch.start.prototype.__proto__.propertyIsEnumerable() ? ",
+    stopwatch.start.prototype.__proto__.propertyIsEnumerable()
+  );
+
+if (stopwatch) {
+  console.log(
+    "typeof stopwatch[Symbol.iterator] === 'function' ?? ",
+    typeof stopwatch[Symbol.iterator] === "function"
+  );
+}
+
+const str = "7amda";
+console.log(
+  "str:",
+  str,
+  ", typeof str[Symbol.iterator] === 'function' ?? ",
+  typeof str[Symbol.iterator] === "function"
+);
+
+const array = [1, 2, 3];
+console.log(
+  "str:",
+  array,
+  ", typeof str[Symbol.iterator] === 'function' ?? ",
+  typeof array[Symbol.iterator] === "function"
+);
