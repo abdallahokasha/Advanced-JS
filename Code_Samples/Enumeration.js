9f864b931e9b23655ad136da6689b352c9b2993c
abdallahokasha <okasha.fci@gmail.com>
2019-10-05 19:41:21 +0200

    Added example for propertyIsEnumerable.

--- Code_Samples/Enumeration.js
+++ Code_Samples/Enumeration.js
@@ -15,3 +15,40 @@ const keys = Object.keys(circle);
 console.log("circle keys: ", keys);
 
 if ("radius" in circle) console.log("circle has radius.");
+// --------------------------
+
+const object1 = {};
+const array1 = [];
+object1.property1 = 42;
+array1[0] = 42;
+
+console.log("object1 ", object1);
+
+console.log("array1 ", array1);
+console.log(
+  "object1.propertyIsEnumerable('property1') ? ",
+  object1.propertyIsEnumerable("property1")
+);
+// expected output: true
+
+console.log(
+  "typeof array1.__proto__[Symbol.iterator] === 'function' ? ",
+  typeof array1.__proto__[Symbol.iterator] === "function"
+);
+
+console.log(
+  "typeof array1[Symbol.iterator] === 'function' ? ",
+  typeof array1[Symbol.iterator] === "function"
+);
+
+console.log(
+  "array1.propertyIsEnumerable(0)) ? ",
+  array1.propertyIsEnumerable(0)
+);
+// expected output: true
+
+console.log(
+  "array1.propertyIsEnumerable('length') ? ",
+  array1.propertyIsEnumerable("length")
+);
+// expected output: false
